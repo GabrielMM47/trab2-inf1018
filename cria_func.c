be7d90acf9f5d311aaec06b3cb5f08d2e8739211
GabrielMM47 <gabrielmmendes@outlook.com>
2024-12-01 20:52:46 -0300

    Nosso código e o teste

--- cria_func.c
+++ cria_func.c
@@ -6,82 +6,91 @@
 /* Gabriel Martins Mendes - 2311271 - Turma 3WC*/
 /* Leo Klinger Svartman - 2310862 - Turma 3WC*/
 
-void finalizacao(unsigned char* c, int* i); 
-void prologo(unsigned char* c, int* i);
-void movrdirsi(unsigned char* c, int* i);
-void movrdirdx(unsigned char* c, int* i);
-void movrsirdx(unsigned char* c, int* i);
+void moverdi2rsi(unsigned char* codigo, int* ind){
+    // mov %rdi, %rsi
+    codigo[(*ind)++] = 0x48;
+    codigo[(*ind)++] = 0x89;
+    codigo[(*ind)++] = 0xfe; 
+}
 
+void moversi2rdx(unsigned char* codigo, int* ind){
+    // mov %rsi, %rdx
+    codigo[(*ind)++] = 0x48;
+    codigo[(*ind)++] = 0x89;
+    codigo[(*ind)++] = 0xf2; 
+}
 
-void cria_func(void *f, DescParam params[], int n, unsigned char codigo[])
-{
+void moverdi2rdx(unsigned char* codigo, int* ind){
+    // mov %rdi, %rdx
+    codigo[(*ind)++] = 0x48;
+    codigo[(*ind)++] = 0x89;
+    codigo[(*ind)++] = 0xfa; 
+}
+
+void cria_func(void *f, DescParam params[], int n, unsigned char codigo[]){
+
     printf("Endereço recebido: %p\n", f);
     // TA COM BASTANTE COMENTARIO PARA A GENTE NÃO SE PERDER... (DEPOIS TIRAMOS ALGUNS
-    int indice = 0;
+    int ind = 0;
 
-    prologo(codigo, &indice);
+    //ALINHANDO A PILHA
+    codigo[ind++] = 0x55;
+    codigo[ind++] = 0x48;
+    codigo[ind++] = 0x89;
+    codigo[ind++] = 0xe5;
+    codigo[ind++] = 0x48; 
+    codigo[ind++] = 0x83;
+    codigo[ind++] = 0xec;
+    codigo[ind++] = 0x10;
 
-    // movq %rdi, -8(%rbp), PROVAVELMENTE NEM PRECISA, JÁ QUE ESTAMOS COPIANDO O VALOR NA MÃO GRANDE, DEPOIS REVER ESSE PARTE... (A NECESSIDADE)
-    codigo[indice++] = 0x48;
-    codigo[indice++] = 0x89;
-    codigo[indice++] = 0x7d;
-    codigo[indice++] = 0xf8;
-    // end movq
+    codigo[ind++] = 0x48;
+    codigo[ind++] = 0xb8;                     // movabs constante 64bits, r12
+    memcpy(&codigo[ind], &f, sizeof(void *)); // Passa o endereço da função para o código
+    ind += sizeof(void *);
 
 
     // tem que ver se está tudo correto, pois quem chama pode não passar todos os parâmetros, o que desalinha 
     int contaPARAM = 0;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         if (params[i].orig_val == PARAM)
             contaPARAM++;
     }
 
-    if (contaPARAM != n)
-    {
-        if (n == 2)
-        {
-            if (params[0].orig_val != PARAM)
-            {
+    if (contaPARAM != n){
+        if (n == 2){
+            if (params[0].orig_val != PARAM){
                 // tenho que mover de di para si
-                movrdirsi(codigo, &indice);
+                moverdi2rsi(codigo, &ind);
             }
         }
 
-        if (n == 3)
-        {
-            if(contaPARAM == 1)
-            {
-                if (params[0].orig_val != PARAM)
-                {
-                    if (params[1].orig_val == PARAM)
-                    {
+        if (n == 3){
+            if(contaPARAM == 1){
+                if (params[0].orig_val != PARAM){
+                    if (params[1].orig_val == PARAM){
                         // tenho que mover de di para si
-                        movrdirsi(codigo, &indice);
+                        moverdi2rsi(codigo, &ind);
                     }
-                    else if (params[2].orig_val == PARAM)
-                    {
+                    else if (params[2].orig_val == PARAM){
                         // Movo de rdi para rdx
-                        movrdirdx(codigo, &indice);
+                        moverdi2rdx(codigo, &ind);
                     }
                 }
                 // nao tem else, pois está no lugar certo.
             }
             else // 2 parametros, 3 não pode pq não é != n
             {
-                if (params[0].orig_val != PARAM)
-                {
+                if (params[0].orig_val != PARAM){
                     // tenho que shiftar todos para a direita
                     // fazendo si --> dx primeiro
-                    movrsirdx(codigo, &indice);
+                    moversi2rdx(codigo, &ind);
 
                     // Movo de rdi para rsi
-                    movrdirsi(codigo, &indice);
+                    moverdi2rsi(codigo, &ind);
                 }
-                if (params[1].orig_val != PARAM)
-                {
+                if (params[1].orig_val != PARAM){
                     // so tenho que mover um, pois significa que o outro ta no lugar certo (di)
-                    movrsirdx(codigo, &indice);
+                    moversi2rdx(codigo, &ind);
                 }
             }
         }
@@ -108,73 +117,66 @@ void cria_func(void *f, DescParam params[], int n, unsigned char codigo[])
     */
 
     // copiar todos os parâmetros para os registradores corretos
-    for (int i = 0; i < n; i++)
-    {
-        switch (params[i].tipo_val)
-        {
+    for (int i = 0; i < n; i++){
+        switch (params[i].tipo_val){
         case INT_PAR:
-            switch (params[i].orig_val)
-            {
+            switch (params[i].orig_val){
             case PARAM:
                 // ja estão no lugar certo :)
                 continue;
                 break;
             case FIX:
                 if (i == 0)
-                    codigo[indice++] = 0xbf; // edi
+                    codigo[ind++] = 0xbf; // edi
                 else if (i == 1)
-                    codigo[indice++] = 0xbe; // esi
+                    codigo[ind++] = 0xbe; // esi
                 else if (i == 2)
-                    codigo[indice++] = 0xba; // edx
+                    codigo[ind++] = 0xba; // edx
 
-                memcpy(&codigo[indice], &params[i].valor.v_int, sizeof(int));
-                indice += sizeof(int);
+                memcpy(&codigo[ind], &params[i].valor.v_int, sizeof(int));
+                ind += sizeof(int);
                 // copia bem sucedida
 
                 break;
             case IND:
                 // o que eu movo aqui é o valor do ponteiro para r9 e depois (r9) para o respectivo reg
                 int *endereco = (int *)params[i].valor.v_ptr;
-                if (i == 0)
-                {
+                if (i == 0){
                     // vou mover esse endereço para r9 se for o primeiro param
-                    codigo[indice++] = 0x49;
-                    codigo[indice++] = 0xb9;
-                    memcpy(&codigo[indice], &endereco, sizeof(int *));
-                    indice += sizeof(int *);
-                    codigo[indice++] = 0x41;
-                    codigo[indice++] = 0x8b;
-                    codigo[indice++] = 0x39;
+                    codigo[ind++] = 0x49;
+                    codigo[ind++] = 0xb9;
+                    memcpy(&codigo[ind], &endereco, sizeof(int *));
+                    ind += sizeof(int *);
+                    codigo[ind++] = 0x41;
+                    codigo[ind++] = 0x8b;
+                    codigo[ind++] = 0x39;
                 }
-                else if (i == 1)
-                {
+                else if (i == 1){
                     // vou mover esse endereço para r10 se for o segundo param
-                    codigo[indice++] = 0x49;
-                    codigo[indice++] = 0xba;
-                    memcpy(&codigo[indice], &endereco, sizeof(int *));
-                    indice += sizeof(int *);
-                    codigo[indice++] = 0x41;
-                    codigo[indice++] = 0x8b;
-                    codigo[indice++] = 0x32;
+                    codigo[ind++] = 0x49;
+                    codigo[ind++] = 0xba;
+                    memcpy(&codigo[ind], &endereco, sizeof(int *));
+                    ind += sizeof(int *);
+                    codigo[ind++] = 0x41;
+                    codigo[ind++] = 0x8b;
+                    codigo[ind++] = 0x32;
                 }
-                else if (i == 2)
-                {
+                else if (i == 2){
                     // vou mover esse endereco para r11 se for o terceiro param
-                    codigo[indice++] = 0x49;
-                    codigo[indice++] = 0xbb;
-                    memcpy(&codigo[indice], &endereco, sizeof(int *));
-                    indice += sizeof(int *);
-                    codigo[indice++] = 0x41;
-                    codigo[indice++] = 0x8b;
-                    codigo[indice++] = 0x13;
+                    codigo[ind++] = 0x49;
+                    codigo[ind++] = 0xbb;
+                    memcpy(&codigo[ind], &endereco, sizeof(int *));
+                    ind += sizeof(int *);
+                    codigo[ind++] = 0x41;
+                    codigo[ind++] = 0x8b;
+                    codigo[ind++] = 0x13;
                 }
                 break;
             }
             break;
 
         case PTR_PAR:
-            switch (params[i].orig_val)
-            {
+            switch (params[i].orig_val){
             case PARAM:
                 // TODO
                 // já era aqui :)
@@ -182,60 +184,54 @@ void cria_func(void *f, DescParam params[], int n, unsigned char codigo[])
                 break;
 
             case FIX:
-                if (i == 0)
-                {
-                    codigo[indice++] = 0x48;
-                    codigo[indice++] = 0xbf; // rdi
+                if (i == 0){
+                    codigo[ind++] = 0x48;
+                    codigo[ind++] = 0xbf; // rdi
                 }
-                else if (i == 1)
-                {
-                    codigo[indice++] = 0x48;
-                    codigo[indice++] = 0xbe; // rsi
+                else if (i == 1){
+                    codigo[ind++] = 0x48;
+                    codigo[ind++] = 0xbe; // rsi
                 }
-                else if (i == 2)
-                {
-                    codigo[indice++] = 0x48;
-                    codigo[indice++] = 0xba; // rdx
+                else if (i == 2){
+                    codigo[ind++] = 0x48;
+                    codigo[ind++] = 0xba; // rdx
                 }
-                memcpy(&codigo[indice], &params[i].valor.v_ptr, sizeof(void *));
+                memcpy(&codigo[ind], &params[i].valor.v_ptr, sizeof(void *));
                 printf("Endereço recebido em cria_func: %p\n", params[i].valor.v_ptr);
-                indice += sizeof(void *);
+                ind += sizeof(void *);
                 break;
 
             case IND:
                 void* endereco = params[i].valor.v_ptr;
-                if (i == 0)
-                {
+                if (i == 0){
                     // move para r9 e desreferencia em rdi
-                    codigo[indice++] = 0x49;
-                    codigo [indice++] = 0xb9;
-                    memcpy(&codigo[indice], &endereco, sizeof(void*));
-                    indice += sizeof(void*);
-                    codigo[indice++] = 0x49;
-                    codigo[indice++] = 0x8b;
-                    codigo[indice++] = 0x39;
+                    codigo[ind++] = 0x49;
+                    codigo [ind++] = 0xb9;
+                    memcpy(&codigo[ind], &endereco, sizeof(void*));
+                    ind += sizeof(void*);
+                    codigo[ind++] = 0x49;
+                    codigo[ind++] = 0x8b;
+                    codigo[ind++] = 0x39;
                 }
-                else if (i == 2)
-                {
+                else if (i == 2){
                     // move para r10 e desreferencia em rsi
-                    codigo[indice++] = 0x49;
-                    codigo [indice++] = 0xba;
-                    memcpy(&codigo[indice], &endereco, sizeof(void*));
-                    indice += sizeof(void*);
-                    codigo[indice++] = 0x49;
-                    codigo[indice++] = 0x8b;
-                    codigo[indice++] = 0x32;
+                    codigo[ind++] = 0x49;
+                    codigo [ind++] = 0xba;
+                    memcpy(&codigo[ind], &endereco, sizeof(void*));
+                    ind += sizeof(void*);
+                    codigo[ind++] = 0x49;
+                    codigo[ind++] = 0x8b;
+                    codigo[ind++] = 0x32;
                 }
-                else
-                {
+                else{
                     // move para r11 e desreferencia em rdx
-                    codigo[indice++] = 0x49;
-                    codigo [indice++] = 0xbb;
-                    memcpy(&codigo[indice], &endereco, sizeof(void*));
-                    indice += sizeof(void*);
-                    codigo[indice++] = 0x49;
-                    codigo[indice++] = 0x8b;
-                    codigo[indice++] = 0x13;
+                    codigo[ind++] = 0x49;
+                    codigo [ind++] = 0xbb;
+                    memcpy(&codigo[ind], &endereco, sizeof(void*));
+                    ind += sizeof(void*);
+                    codigo[ind++] = 0x49;
+                    codigo[ind++] = 0x8b;
+                    codigo[ind++] = 0x13;
                 }
                 break;
             }
@@ -243,76 +239,11 @@ void cria_func(void *f, DescParam params[], int n, unsigned char codigo[])
         }
     }
 
-    // ESTAVA PERDENDO A REFERENCIA PARA A FUNCAO, ESTÃO COPIEI NA MÃO GRANDE
-    codigo[indice++] = 0x48;
-    codigo[indice++] = 0xb8;                     // movq %rax, <endereço da função>, o compilar coloca como movabs...
-    memcpy(&codigo[indice], &f, sizeof(void *)); // Copia o endereço da função para o código
-    indice += sizeof(void *);
+    // CHAMADA DA FUNÇÃO COM O CALL INDIRETO (call *%rax)
+    codigo[ind++] = 0xff;
+    codigo[ind++] = 0xd0;
 
-    // call *%rax (endereco da funcao chamada) para fazer as operacoes que devem ser feitas, por exemplo, como seriam feitas na mult ou na memcmp...
-    codigo[indice++] = 0xff;
-    codigo[indice++] = 0xd0;
-    // end call
-
-    // finalização
-    finalizacao(codigo, &indice);
-    // end finalização
-
-    printf("Código gerado:\n");
-    for (int j = 0; j < indice; j++)
-    {
-        printf("%02x ", codigo[j]);
-    }
-    printf("\n");
-
-    FILE *fout = fopen("codigo.bin", "wb");
-    if (fout == NULL)
-    {
-        perror("Erro ao abrir o arquivo para escrita");
-        return;
-    }
-    fwrite(codigo, sizeof(unsigned char), indice, fout);
-    fclose(fout);
-
-    printf("Código gerado gravado em 'codigo.bin'.\n");
-}
-
-void finalizacao(unsigned char *codigo, int *indice) {
-    codigo[(*indice)++] = 0xc9; // leave
-    codigo[(*indice)++] = 0xc3; // ret    
-}
-
-void prologo(unsigned char *codigo, int *indice)
-{
-    // prologo da funcao, i.e., pushq rbp, movq rsp rbp, subq 32 (pode ser necessario aumentar)
-    codigo[(*indice)++] = 0x55;
-    codigo[(*indice)++] = 0x48;
-    codigo[(*indice)++] = 0x89;
-    codigo[(*indice)++] = 0xe5;
-    codigo[(*indice)++] = 0x48; // subq 32 rsp
-    codigo[(*indice)++] = 0x83;
-    codigo[(*indice)++] = 0xec;
-    codigo[(*indice)++] = 0x20;
-    // end prologo
-}
-
-void movrdirsi(unsigned char* codigo, int* indice)
-{
-    codigo[(*indice)++] = 0x48;
-    codigo[(*indice)++] = 0x89;
-    codigo[(*indice)++] = 0xfe; // mov %rdi, %rsi
-}
-
-void movrsirdx(unsigned char* codigo, int* indice)
-{
-    codigo[(*indice)++] = 0x48;
-    codigo[(*indice)++] = 0x89;
-    codigo[(*indice)++] = 0xf2; // mov %rsi, %rdx
-}
-
-void movrdirdx(unsigned char* codigo, int* indice)
-{
-    codigo[(*indice)++] = 0x48;
-    codigo[(*indice)++] = 0x89;
-    codigo[(*indice)++] = 0xfa; // mov %rdi, %rdx
+    // FINALIZANDO 
+    codigo[ind++] = 0xc9; // leave
+    codigo[ind++] = 0xc3; // ret  
 }
